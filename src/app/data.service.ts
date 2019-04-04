15ba35cf1b0a05b4e691bacd93204232786792aa
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-04-04 17:05:07 +0200

    [UPDATE] - Replaced my thai start url hard coded to environment variable

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MY_THAI_STAR_DISH } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  myThaiStarDish = 'http://de-mucdevondepl01:8090/api/services/rest/dishmanagement/v1/dish/1';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getDishes() {
-    return this.http.get(
-      this.myThaiStarDish,
-    );
+    return this.http.get(MY_THAI_STAR_DISH);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
 
+export const MY_THAI_STAR_DISH =
+  'http://de-mucdevondepl01:8090/api/services/rest/dishmanagement/v1/dish/1';
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
